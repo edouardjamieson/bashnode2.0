e85ef6a6afc43f830d23b723f281052b30e9cac4
Charles-Édouard Jamieson <edouardjamieson99@gmail.com>
2024-11-16 10:24:12 -0500

    Made DB server only + built initial prisma schema

--- supabase/server.ts
+++ supabase/server.ts
@@ -6,7 +6,7 @@ export async function createClient() {
 
 	// Create a server's supabase client with newly configured cookie,
 	// which could be used to maintain user's session
-	return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+	return createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
 		cookies: {
 			getAll() {
 				return cookieStore.getAll();
